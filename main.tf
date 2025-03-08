e42fc991304c616b0b5f8a9ffd4e5553ee2b860a
ztarch <zerotrustarchitect@gmail.com>
2025-03-07 20:11:09 -0500

    ok

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
+#resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
 #  instance_type = "t3.nano"
    instance_type = var.instance_type
